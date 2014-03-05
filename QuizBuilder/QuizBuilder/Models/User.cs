eda794500c8014a0de24bcdd7e426b43032e9138
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-05 15:42:07 -0500

    Added Annotations to Site

--- QuizBuilder/QuizBuilder/Models/User.cs
+++ QuizBuilder/QuizBuilder/Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizBuilder.Models
 {
@@ -13,7 +14,9 @@ namespace QuizBuilder.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         public bool IsAdmin { get; set; }
         public virtual ICollection<QuizAttempt> QuizAttempts { get; set; }
